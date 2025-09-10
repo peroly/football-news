a03ba58e4082c1534fcd93daefd0191a700ea578
peroly <mernawatispeed08@gmail.com>
2025-09-10 14:28:39 +0700

    tutorial 2

--- main/views.py
+++ main/views.py
@@ -10,7 +10,8 @@ def show_main(request):
     context = {
         'npm' : '2406352304',
         'name': 'Afero Aqil Roihan',
-        'class': 'PBP D'
+        'class': 'PBP D',
+        'news_list': news_list,
     }
 
     return render(request, "main.html", context)
